da5e4bf91574cd1ddaddde5623a08fb028a7409d
thomasmaliappis <thomasmaliappis@gmail.com>
2023-10-09 13:48:36 +0200

    changed details shown in admin page

--- pizza/admin.py
+++ pizza/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Pizza, Size
 
-admin.site.register(Pizza)
+
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ('topping1', 'topping2', 'size',)
+
+
+admin.site.register(Pizza, PizzaAdmin)
 admin.site.register(Size)
